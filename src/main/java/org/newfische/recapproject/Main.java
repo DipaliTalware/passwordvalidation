ac513bd479cbf0f965f4f28f839e53581025e174
DipaliTalware <d.talware@gmail.com>
2024-09-03 12:23:13 +0200

    Initial commit

--- src/main/java/org/newfische/recapproject/Main.java
+++ src/main/java/org/newfische/recapproject/Main.java
@@ -2,6 +2,6 @@ package org.newfische.recapproject;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!");
     }
 }
